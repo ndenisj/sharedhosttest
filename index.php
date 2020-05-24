87c1bc6bddfabf84d30a4ced188218ff4fa870be
Denis John <45005671+ndenisj@users.noreply.github.com>
2020-05-24 14:24:36 +0100

    create .php file

---
+++ index.php
@@ -0,0 +1,11 @@
+  
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Git Version control Test</title>
+</head>
+<body>
+	<h1>This is a version control test for a shared hosting pipeline</h1>
+  <?php echo "DENIS"; ?>
+</body>
+</html>
