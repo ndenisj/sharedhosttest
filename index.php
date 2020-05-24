a6f0a1093ec6cfa48e90a4243b59e7c42b83acb2
Denis John <45005671+ndenisj@users.noreply.github.com>
2020-05-24 14:59:43 +0100

    update index

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>Git Version control Test</title>
 </head>
 <body>
-	<h1>This is a version control test for a shared hosting pipeline</h1>
+	<h1>This is a version control test for a shared hosting pipeline</h1> 22
   <?php echo "DENIS"; ?>
 </body>
 </html>
